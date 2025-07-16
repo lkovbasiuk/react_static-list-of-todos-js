60337bc73db5622a8c88c1f4a03b6fc923ba6125
Liudmyla Kovbasiuk <lchubatiuk96@gmail.com>
2025-07-16 15:46:56 +0200

    Added changes

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,7 +4,6 @@ import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
-    key={todo.id}
     className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
